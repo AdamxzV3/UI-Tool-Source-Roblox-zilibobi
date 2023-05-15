e0dbbd09c27e93ee9fc18bc20dbda800522ae637
CodePulse <99908796+AdamxzV3@users.noreply.github.com>
2023-05-15 16:21:23 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,20 @@
+return {
+	-- scrolling smooth factor
+	scrollingFactor = 0.3,
+
+	-- button effects
+	buttonDampingRatio = 0.5,
+	buttonUndampedFrequency = 3,
+
+	buttonPositionChange = 2,
+	buttonSizeChange = 1,
+
+	buttonEffectDelay = 0.1,
+
+	-- lock state (variable)
+	lock = false,
+	lockWindow = "",
+
+	-- accent (variable)
+	accent = "red",
+}
